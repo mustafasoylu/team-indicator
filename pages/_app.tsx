7062d729e234e92e21319d7c98452e0361edcdb3
Mustafa Soylu <m.soylu@trajectory.jp>
2022-06-14 21:43:14 +0900

    add complementary pages

---
+++ pages/_app.tsx
@@ -0,0 +1,14 @@
+import { SessionProvider } from 'next-auth/react';
+import '../public/spinner.css';
+import { AppProps } from 'next/app';
+
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
+}
